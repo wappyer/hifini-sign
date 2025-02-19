f26f2f0faa4ad69875b43288c5d12dd1677b627c
wappyer <1059015328@qq.com>
2025-02-19 18:22:33 +0800

    add sign

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+)
+
+const UrlIndex = "https://www.hifini.com/"
+const UrlSignIn = "https://www.hifini.com/sg_sign.htm"
+
+func main() {
+	var token = ""
+	flag.StringVar(&token, "token", "", "bbs_token")
+	flag.Parse()
+	if token == "" {
+		token = os.Getenv("token")
+		if token == "" {
+			log.Printf("token is empty")
+			return
+		}
+	}
+
+	sign := getSign(token)
+	SignIn(token, sign)
+}
+
+// 获取sign(sign为签到接口重要传参)
+func getSign(token string) (sign string) {
+	client := http.Client{}
+	req, err := http.NewRequest("GET", UrlIndex, nil)
+	req.Header.Set("Cookie", fmt.Sprintf("bbs_token=%s", token))
+	indexResp, err := client.Do(req)
+	if err != nil {
+		log.Printf("[getSign]请求首页失败，err: %v\n", err)
+		return
+	}
+	defer indexResp.Body.Close()
+
+	indexHtmlByte, err := io.ReadAll(indexResp.Body)
+	if err != nil {
+		log.Printf("[getSign]读取首页内容失败，err: %v\n", err)
+		return
+	}
+
+	reg, err := regexp.Compile(`var sign = "(.*)"`)
+	if err != nil {
+		log.Printf("[getSign]正则匹配表达式错误，err: %v\n", err)
+		return
+	}
+	match := reg.FindStringSubmatch(string(indexHtmlByte))
+	if len(match) < 2 {
+		log.Printf("[getSign]正则匹配未获取到sign，匹配结果:%v\n", match)
+		return
+	}
+	sign = match[1]
+	return
+}
+
+func SignIn(token, sign string) {
+	client := http.Client{}
+	body := strings.NewReader(fmt.Sprintf("sign=%s", sign))
+	req, err := http.NewRequest("POST", UrlSignIn, body)
+	req.Header.Set("Cookie", fmt.Sprintf("bbs_token=%s", token))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8")
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Printf("[SignIn]签到请求失败，err: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+	log.Println("[SignIn]签到请求成功")
+	return
+}
